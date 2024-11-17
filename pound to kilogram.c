fde1071437c5c4ba0bcfa6ba09670d972c13c2d5
jaidrum <jaidrumkiome4@gmail.com>
2024-11-18 01:49:22 +0300

    Update pound to kilogram.c

--- pound to kilogram.c
+++ pound to kilogram.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    float pounds, kilograms;
-    float start, end, step;
-
-    printf("Enter starting Pounds value: ");
-    scanf("%f", &start);
-    printf("Enter ending Pounds value: ");
-    scanf("%f", &end);
-    printf("Enter increment step: ");
-    scanf("%f", &step);
-
-    printf("\nPounds to Kilograms Table\n");
-    printf("----------------------------\n");
-    printf(" Pounds  | Kilograms\n");
-    printf("----------------------------\n");
-
-    for (pounds = start; pounds <= end; pounds += step) {
-        kilograms = pounds * 0.453592;
-        printf("%8.1f | %9.3f\n", pounds, kilograms);
-    }
-
+int main() 
+{
+    float a;
+    float weight;
+    printf("Enter weight value in pounds:");
+    scanf("%f",& weight);
+    a=weight*0.453592;
+    printf("Value of weight in kg:%.2f",a);
     return 0;
 }
