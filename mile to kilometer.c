2e5b6c5f28f2d365e98cd27847093ff71359d77d
jaidrum <jaidrumkiome4@gmail.com>
2024-11-18 01:56:24 +0300

    Update mile to kilometer.c

--- mile to kilometer.c
+++ mile to kilometer.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    float miles, kilometers;
-    float start, end, step;
-
-    printf("Enter starting Miles value: ");
-    scanf("%f", &start);
-    printf("Enter ending Miles value: ");
-    scanf("%f", &end);
-    printf("Enter increment step: ");
-    scanf("%f", &step);
-
-    printf("\nMiles to Kilometers Table\n");
-    printf("----------------------------\n");
-    printf("  Miles  | Kilometers\n");
-    printf("----------------------------\n");
-
-    for (miles = start; miles <= end; miles += step) {
-        kilometers = miles * 1.60934;
-        printf("%8.1f | %10.3f\n", miles, kilometers);
-    }
-
+int main() 
+{
+    float a;
+    float mile;
+    printf("Enter the value of distance in miles:");
+    scanf("%f",&mile);
+    a=mile*1.60934;
+    printf("Value of distance in Km:%.2f.\n",a);
     return 0;
 }
